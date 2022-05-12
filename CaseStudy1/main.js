28f5d64bf3a5b6bfa1cd189214f17a023ec6457b
HiepTranCG <hiep.tran@codegym.vn>
2022-05-12 18:02:56 +0700

    move validate

--- CaseStudy1/main.js
+++ CaseStudy1/main.js
@@ -45,6 +45,7 @@ class Player {
     }
 
     move(x, y) {
+
         this.x += x;
         this.y += y;
         this.draw();
@@ -132,7 +133,7 @@ class Particle {
 // -------------------------- Main logic --------------------------
 
 // Creating Player Object, Weapons Array, Enemy Array, Etc Array
-const abhi = new Player(playerPosition.x, playerPosition.y,15,"white");
+const abhi = new Player(playerPosition.x, playerPosition.y, 15, "white");
 const weapons = [];
 const enemies = [];
 const particles = [];
@@ -147,7 +148,7 @@ const spawnEnemy = () => {
     let random;
 
     // Making Enemy Location Random but only from outsize of screen
-    if(Math.random() < 0.5) {
+    if (Math.random() < 0.5) {
         // making X equal to very left off of screen or very right off of screen and setting Y to any where vertically
         random = {
             x: Math.random() < 0.5 ? canvas.width + enemySize : 0 - enemySize,
@@ -164,8 +165,8 @@ const spawnEnemy = () => {
 
     // finding Angle between center (means Player position) and enemy position
     const myAngle = Math.atan2(
-        canvas.height / 2 - random.y,
-        canvas.width / 2 - random.x
+        abhi.y - random.y,
+        abhi.x - random.x
     );
 
     // making velocity or speed of enemy
@@ -206,12 +207,13 @@ const gameoverLoader = () => {
 
 // ------------------------ Creating Animation Function ------------------------
 let animationId;
+
 function animation() {
     // Making recursion
     animationId = requestAnimationFrame(animation);
 
     // clearing canvas on each frame
-    context.fillStyle="rgba(49, 49, 49, 0.4)";
+    context.fillStyle = "rgba(49, 49, 49, 0.4)";
 
     context.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -220,7 +222,7 @@ function animation() {
 
     // Generating Particles
     particles.forEach((particle, particleIndex) => {
-        if(particle.alpha <= 0) {
+        if (particle.alpha <= 0) {
             particles.splice(particleIndex, 1);
         } else {
             particle.update();
@@ -232,12 +234,12 @@ function animation() {
         weapon.update();
 
         // Removing Weapons if they are off screen
-        if(
+        if (
             weapon.x + weapon.radius < 1 ||
             weapon.y + weapon.radius < 1 ||
             weapon.x - weapon.radius > canvas.width ||
             weapon.y - weapon.radius > canvas.height
-            ) {
+        ) {
             weapons.splice(weaponIndex, 1);
         }
     })
@@ -253,7 +255,7 @@ function animation() {
         );
 
         // Stop Game if enemy hit player
-        if(distanceBetweenPlayerAndEnemy - abhi.radius - enemy.radius < 1) {
+        if (distanceBetweenPlayerAndEnemy - abhi.radius - enemy.radius < 1) {
             cancelAnimationFrame(animationId);
 
             return gameoverLoader();
@@ -266,21 +268,20 @@ function animation() {
                 weapon.y - enemy.y
             );
 
-            if(distanceBetweenWeaponAndEnemy - weapon.radius - enemy.radius < 1) {
+            if (distanceBetweenWeaponAndEnemy - weapon.radius - enemy.radius < 1) {
                 // reducing Size of enemy on hit
-                if(enemy.radius > 18) {
+                if (enemy.radius > 18) {
                     enemy.radius -= 10;
                     weapons.splice(weaponIndex, 1);
 
                     // increasing player score when shoot
                     playerScore += 5;
-
                     // Rendering player Score in scoreboard html element
                     scoreBoard.innerHTML = `Score: ${playerScore}`;
                 }
                 // Removing enemy on hit if they are below 18
                 else {
-                    for(let i = 0; i < enemy.radius*5; i++) {
+                    for (let i = 0; i < enemy.radius * 5; i++) {
                         particles.push(
                             new Particle(weapon.x, weapon.y, Math.random() * 2, enemy.color, {
                                 x: (Math.random() - 0.5) * (Math.random() * 7),
@@ -307,14 +308,13 @@ function animation() {
 }
 
 
-
 // ------------------------ Adding Event Listeners ------------------------
 // event Listener for Light Weapon aka left click
-canvas.addEventListener("click", (e)=> {
-    // find angle between player position(center) and click co-ordinates
+canvas.addEventListener("click", (e) => {
+    // find angle between player position and click co-ordinates
     const myAngle = Math.atan2(
-        e.clientY - canvas.height / 2,
-        e.clientX - canvas.width / 2,
+        e.clientY - abhi.y,
+        e.clientX - abhi.x,
     );
 
     // making const speed for light weapon
@@ -324,8 +324,35 @@ canvas.addEventListener("click", (e)=> {
     };
 
     // adding light weapon in weapons array
-    weapons.push(new Weapon(canvas.width / 2, canvas.height / 2,6,"white", velocity));
+    weapons.push(new Weapon(abhi.x, abhi.y, 6, "white", velocity));
 });
 
+// event Listener for moving player
+addEventListener("keypress", (e) => {
+    switch (e.key) {
+        case "a":
+            if(abhi.x + abhi.radius > 40)
+                abhi.move(-15, 0);
+            break;
+        case "w":
+            if(abhi.y + abhi.radius > 50)
+                abhi.move(0, -15);
+            break;
+        case "d":
+            if(abhi.x + abhi.radius < canvas.width - 15)
+                abhi.move(15, 0);
+            break;
+        case "s":
+            if(abhi.y + abhi.radius < canvas.height - 15)
+                abhi.move(0, 15);
+            break;
+    }
+
+});
+
+addEventListener("resize", () => {
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+})
 
 animation();
